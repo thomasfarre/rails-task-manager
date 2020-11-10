8a1e61d91f26ba8d5f44260175cd4090475a61b4
thomasfarre <th.farre@gmail.com>
2020-11-10 14:22:07 +0100

    exo 2 finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get "tasks", to: "tasks#index", as: :index_task
+  get "tasks/new", to: "tasks#new", as: :new_task
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  post "tasks", to: "tasks#create"
+  delete "tasks/:id", to: "tasks#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
